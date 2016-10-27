4caa4df0e143ffe85aef6138065608b45b1ad865
Sophie-Jeanne la Douce <sophie.jeanne.la.douce@gmail.com>
2016-10-27 04:00:24 -0700

    Move files out of the old mushed-together mess.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# occupancy-web-app
-Occupancy Web App
+# Occupancy Web App (Server)
+
+This is the NodeJS web server for the occupancy web app, 
+which provides a real-time directory for the rooms.
+
+
+
+## Port Use
+
+<table>
+    <tr>
+        <th>Port</th>
+        <th>External</th>
+        <th>Description</th>
+    </tr>
+    <tr>
+        <td>3002</td> <td>Yes</td> <td>Occupancy Web App</td>
+    </tr>
+</table>
+
+
---
+++ app.js
@@ -0,0 +1,28 @@
+/// <reference path="../typings/node.d.ts" />
+const debug = require('debug')('app:main');
+const nconf = require('nconf');
+////////////////////////////////////////////////////////////////////////////////
+//
+// Configuration
+//
+////////////////////////////////////////////////////////////////////////////////
+nconf.argv().env().defaults({
+    'port': 3002,
+});
+const port = nconf.get('port');
+debug('port: ' + port);
+////////////////////////////////////////////////////////////////////////////////
+//
+// Set up web server.
+//
+// Serve static files from ./static
+//
+////////////////////////////////////////////////////////////////////////////////
+const express = require('express'); // call express
+const compression = require('compression'); // Enable compression
+const serveStatic = require('serve-static'); // Serve static files.
+const app = express(); // define our app using express
+app.use(compression());
+app.use(serveStatic(__dirname + '/static', { index: ['index.html'] }));
+app.listen(port);
+//# sourceMappingURL=app.js.map
